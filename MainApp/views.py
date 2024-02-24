23474cef58c82b488e69f9d31b11e53a26ebbda4
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-02-24 15:17:11 +0300

    Done. M1:task1

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    text = """<h1>"Изучаем django"</h1>
+        <strong>Автор</strong>: <i>Иванов И.П.</i>
+        """
+    return HttpResponse(text)
