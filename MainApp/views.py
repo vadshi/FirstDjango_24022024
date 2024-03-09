12d6299ec6734b13d1c5162a240826404213468e
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-03-09 15:29:08 +0300

    Done.M4:part2

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,21 +1,10 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
-from MainApp.models import Item
+from MainApp.models import Item, Color
 from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your views here.
-
-
-items = [
-    {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
-    {"id": 2, "name": "Куртка кожаная", "quantity": 2},
-    {"id": 5, "name": "Coca-cola 1 литр", "quantity": 12},
-    {"id": 7, "name": "Картофель фри", "quantity": 0},
-    {"id": 8, "name": "Кепка", "quantity": 124},
-]
-
-
 def home(request):
     context = {
         "name": "Петров Иван Николаевич",
@@ -26,34 +15,32 @@ def home(request):
 
 def about(request):
     author = {
-        "Имя": "Иван",
-        "Отчество": "Петрович",
-        "Фамилия": "Иванов",
-        "телефон": "8-923-600-01-02",
+        "first_name": "Иван",
+        "middle_name": "Петрович",
+        "last_name": "Иванов",
+        "phone": "8-923-600-01-02",
         "email": "vasya@mail.ru",
     }
-    text = f"""
-    <header>
-        / <a href="/"> Home </a> / <a href="/items"> Items </a> / <a href="/about"> About </a>
-    </header>
-    Имя: <b>{author["Имя"]}</b><br>
-    Отчество: <b>{author["Отчество"]}</b><br>
-    Фамилия: <b>{author["Фамилия"]}</b><br>
-    телефон: <b>{author["телефон"]}</b><br>
-    email: <b>{author["email"]}</b><br>
-    """
-    return HttpResponse(text)
+    context = {
+        "author": author
+    }
+    return render(request, "about.html", context)
 
 
 def get_item(request, item_id: int):
     """По указанному id возвращаем имя элемента и кол-во"""
     try:
         item = Item.objects.get(id=item_id)
+        colors = []
+        # Проверяем, что у элемента есть хоть один цвет
+        if item.colors.exists():
+            colors = item.colors.all()
     except ObjectDoesNotExist:
         return HttpResponseNotFound(f"Item with id={item_id} not found.")
     else:
         context = {
-            "item": item
+            "item": item,
+            "colors": colors,
         }
         return render(request, "item_page.html", context)
 
